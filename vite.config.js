0d766b9ea08180590f20756d775092439e8e2835
samuelkenworthy91-byte <samuel.kenworthy91@gmail.com>
2026-06-16 17:00:21 +0000

    Make GitHack build use relative asset paths

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ command, mode }) => ({
-  base: command === 'build' && mode !== 'electron' ? '/KDM/' : './',
+export default defineConfig({
+  base: './',
   plugins: [react()],
-}));
+});
